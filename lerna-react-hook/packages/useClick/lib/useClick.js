b7a09a6a8530254e9686e23d887ad5df51fce547
이주영 <juu0124@wemakeprice.com>
2020-06-10 18:33:53 +0900

    (master): useClick hook 추가

--- lerna-react-hook/packages/useClick/lib/useClick.js
+++ lerna-react-hook/packages/useClick/lib/useClick.js
@@ -1,7 +1,17 @@
 'use strict';
+import { useRef, useEffect } from 'react';
 
 module.exports = useClick;
 
-function useClick() {
-    // TODO
+function useClick(onClick) {
+    if (typeof onClick !== 'function') return;
+
+    const element = useRef();
+    useEffect(() => {
+      element.current && element.current.addEventListenr('click', onClick);
+      return () => {
+        element.current && element.current.removeListener('click', onClick);
+      }
+    }, []);
+    return element;
 }
